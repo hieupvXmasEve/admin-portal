bdd7dbe1cec9e8e02c9556ea83a682d75773c701
hieupv <hieupv2412@gmail.com>
2025-08-15 10:16:06 +0700

    fix: update logo handling and favicon configuration

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -4,12 +4,5 @@ const { getLogoText } = useSystemConfig();
 </script>
 
 <template>
-    <div
-        class="size-6"
-        :style="{
-            WebkitMask: `url(${getLogoText}) no-repeat center / contain`,
-            mask: `url(${getLogoText}) no-repeat center / contain`,
-            backgroundColor: '#3ab60c', // đổi màu ở đây
-        }"
-    ></div>
+    <img :src="getLogoText" alt="Logo">
 </template>
